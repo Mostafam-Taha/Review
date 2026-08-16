2d04ad1946c9c841fdb2d946d1127655c25ef12e
mostafam taha <mostafamta347@gmail.com>
2026-08-17 02:42:01 +0300

    Add files via upload

---
+++ Project ToDo List/To Do List.py
@@ -0,0 +1,55 @@
+import csv
+from datetime import datetime
+
+name_files = "data_todo_list.csv"
+data = []
+def show_menu():
+    print("\n==== Welcome To User ====")
+    print("1. Add To Task")
+    print("2. Show All Tasks")
+    print("0. Exit")
+
+def file_save():
+    with open(name_files, "a", newline="") as create_file:
+        x  = ["name", "created_at", "done"]
+        create_filnalty = csv.DictWriter(create_file, x)
+        create_filnalty.writeheader()
+        create_filnalty.writerows(data)
+
+def add_to_task():
+    add_input_task = input("Please Enter is new task: ")
+
+    created_at = datetime.now().strftime("%Y-%B-%d %H:%M:%S")
+    new_task = {
+        "name": add_input_task,
+        "created_at": created_at,
+        "done": True
+    }
+    data.append(new_task)
+    file_save()
+
+
+def show_all_tasks():
+    with open(name_files, "r") as show_file_reading:
+        reader = csv.DictReader(show_file_reading)
+        for row in reader:
+            print (f"{row["name"]} | {row["created_at"]} | {row["done"]}")
+
+def main():
+    while True:
+        show_menu()
+
+        try:
+            user_input_choose = int(input("\nEnter You number: "))
+        except:
+            print("Chatacter not number")
+            continue
+        if user_input_choose == 1:
+            add_to_task()
+        elif user_input_choose == 2:
+            show_all_tasks()
+        else:
+            print("Thank You!")
+            break
+
+main()
