572bc698d8f5759a56c86ad2f84c2dd80b5619bb
mostafam taha <mostafamta347@gmail.com>
2026-08-20 11:27:56 +0300

    Add files via upload

--- Project Password Manager/password_manager.py
+++ Project Password Manager/password_manager.py
@@ -3,7 +3,12 @@
 # 2. طلب من المستخدم ادخال كلمة المرور فى كل مره يطلب يريد عرض كلمة مرور ما
 # 3. فتح نفاذة القوائم للختيار
 # 4. اذا كانت هناك اي بيانات خطأ لا يتم الارسال إللى قائمة الاختيارات الخاصة بالبرنامج ابدًا
+import os
+import csv
 import random
+from datetime import datetime
+
+file_system_management = "Project Password Manager/password.csv"
 
 class Login_Password_Manager:
     def __init__(self, username, password):
@@ -15,9 +20,23 @@ class Login_Password_Manager:
         
         in_password = input("Enter your password: ")
 
-        In_user_ID = random.randint(1000000,100000000)
-        print(In_user_ID)
-        # هكمل الكود بعد مصحى من النوم 😴 ان شاء الله
+        in_user_ID = random.randint(1000000,1000000000)
 
+        created_at = datetime.now().strftime("%B-%Y-%d %H:%M:%S")
+        new_sign_in_user_manager = {
+            "ID_User": in_user_ID,
+            "Username": in_username,
+            "Password": in_password,
+            "Created_at": created_at
+        }
+
+        fieldnames = ["ID_User", "Username", "Password", "Created_at"]
+        file_exist = os.path.isfile(file_system_management)
+        with open(file_system_management, "a", newline="") as file_as_password_user_manager:
+            file_user = csv.DictWriter(file_as_password_user_manager, fieldnames=fieldnames)
+            if not file_exist:
+                file_user.writeheader()
+            file_user.writerow(new_sign_in_user_manager)
+            
 user = Login_Password_Manager("Mostafa", 1234)
-user.sign_in()
+user.login()
